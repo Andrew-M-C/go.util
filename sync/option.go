bde7180fbd462e637896ea5150f1b897d4f28957
Andrew-M-C <laplacezhang@126.com>
2022-10-19 19:56:24 +0800

    [-] docs: <sync> 删除一些多余的 godoc

--- sync/option.go
+++ sync/option.go
@@ -1,4 +1,3 @@
-// Package sync 提供一些额外的、非常规的 sync 功能
 package sync
 
 import "time"
@@ -9,6 +8,7 @@ type option struct {
 	reentrantNotify ReentrantNotifyFunc
 }
 
+// Option 表示额外参数, 只能通过本 package 调用
 type Option func(opt *option)
 
 // WithRetryInterval 指定重试的时间间隔
@@ -20,10 +20,10 @@ func WithRetryInterval(intvl time.Duration) Option {
 	}
 }
 
-// ReentrantNotifyFunc 表示发生重入时的通知函数
+// ReentrantNotifyFunc 表示发生重入时的通知函数类型
 type ReentrantNotifyFunc func(goroutineID int64)
 
-// WithReentrantNotification 指定发生
+// WithReentrantNotification 指定发生重入时, 调用回调
 func WithReentrantNotification(f ReentrantNotifyFunc) Option {
 	return func(opt *option) {
 		opt.reentrantNotify = f
--- sync/reentrant_lock.go
+++ sync/reentrant_lock.go
@@ -1,4 +1,3 @@
-// Package sync 提供一些额外的、非常规的 sync 功能
 package sync
 
 import (
