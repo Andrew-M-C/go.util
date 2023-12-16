3d1d581b3a6b6cc2bfa5cd889fe1001e6a7273fa
Andrew-M-C <laplacezhang@126.com>
2023-12-16 10:06:01 +0800

    [*] feat: <ids/base36> 限制必须使用 64 位类型数据传递

--- ids/base36/base36.go
+++ ids/base36/base36.go
@@ -1,12 +1,11 @@
-// Package base36
+// Package base36 实现混合字母和数字的数字型 ID 值, 这主要是为了便于人为设定一个可读友好的字符串,
+// 同时又能转换为数字的 ID 存储。不区分大小写, 最多 10 个字符
 package base36
 
 import (
 	"fmt"
 	"strconv"
 	"strings"
-
-	"golang.org/x/exp/constraints"
 )
 
 // FullPrecisionSize uint64 可支持的完整 string 长度
@@ -19,14 +18,19 @@ const Float64PrecisionSize = 10
 // MaxID ID 最大值
 const MaxID = 3656158440062975 // ZZZZZZZZZZ
 
+// Double 表示支持的数据类型
+type Double interface {
+	~float64 | ~int64 | ~uint64
+}
+
 // Itoa 将一个 id 转为 string 类型
-func Itoa[N constraints.Float | constraints.Integer](id N) string {
+func Itoa[N Double](id N) string {
 	s := strconv.FormatUint(uint64(id), 36)
 	return strings.ToUpper(s)
 }
 
 // Atoi 将一个 string 转为数字值
-func Atoi[N constraints.Float | constraints.Integer](s string) (N, error) {
+func Atoi[N Double](s string) (N, error) {
 	if len(s) > Float64PrecisionSize {
 		return 0, fmt.Errorf("string length should not be more than %d", Float64PrecisionSize)
 	}
--- ids/base36/base36_test.go
+++ ids/base36/base36_test.go
@@ -18,17 +18,26 @@ var (
 func TestAtoi(t *testing.T) {
 	cv("Atoi", t, func() {
 		s := "zzzzzzzzzz"
-		i, err := base36.Atoi[int](s)
+		i, err := base36.Atoi[int64](s)
 		so(err, isNil)
 		so(i, eq, base36.MaxID)
+	})
+
+	cv("浮点数精度支持", t, func() {
+		s := "ZZZZZZZZZZ"
+		f, err := base36.Atoi[float64](s)
+		so(err, isNil)
+		so(f, eq, base36.MaxID)
+		so(uint64(f), eq, base36.MaxID)
+		so(int64(f), eq, base36.MaxID)
 	})
 }
 
 func TestItoa(t *testing.T) {
 	cv("Itoa", t, func() {
-		i := int32(1234567890)
+		i := int64(1234567890)
 		s := base36.Itoa(i)
-		ii, err := base36.Atoi[int](s)
+		ii, err := base36.Atoi[int64](s)
 		so(err, isNil)
 		so(ii, eq, i)
 	})
