0e45de8c8b236e9648ec46f632212cb43853050b
Andrew-M-C <laplacezhang@126.com>
2024-05-18 11:58:40 +0800

    [+] <channel> 添加带超时写入或读取 channel 的函数封装

--- channel/channel.go
+++ channel/channel.go
@@ -1,6 +1,8 @@
 // Package channel 提供一些方便的 chan 操作封装
 package channel
 
+import "time"
+
 // WriteNonBlocked 非阻塞地向一个 chan 中写入数据, 当写入失败时返回
 func WriteNonBlocked[T any](ch chan<- T, v T) (full, closed bool) {
 	defer func() {
@@ -11,6 +13,39 @@ func WriteNonBlocked[T any](ch chan<- T, v T) (full, closed bool) {
 
 	select {
 	default:
+		full = true
+	case ch <- v:
+		// OK
+	}
+
+	return
+}
+
+// WriteWithTimeout 带超时向一个 chan 中写入数据, 当时间到了的时候依然写入失败, 说明
+// channel 是满的, 直接返回
+func WriteWithTimeout[T any](ch chan<- T, v T, timeout time.Duration) (full, closed bool) {
+	if timeout <= 0 {
+		return WriteNonBlocked(ch, v)
+	}
+
+	// 检查是否已关闭
+	defer func() {
+		if e := recover(); e != nil {
+			closed = true
+		}
+	}()
+
+	// 创建 timer
+	t := time.NewTimer(timeout)
+	defer func() {
+		if !t.Stop() {
+			// ref: [使用 Golang Timer 的正确方式](http://russellluo.com/2018/09/the-correct-way-to-use-timer-in-golang.html)
+			_, _, _ = ReadNonBlocked(t.C)
+		}
+	}()
+
+	select {
+	case <-t.C:
 		full = true
 	case ch <- v:
 		// OK
@@ -34,4 +69,40 @@ func ReadNonBlocked[T any](ch <-chan T) (v T, empty, emptyAndClosed bool) {
 		return v, true, true
 	}
 	return v, empty, false
+}
+
+// ReadWithTimeout 从一个 chan 中读出数据。当读取超时时返回
+func ReadWithTimeout[T any](ch <-chan T, timeout time.Duration) (v T, emptyAndTimeout, emptyAndClosed bool) {
+	if timeout <= 0 {
+		return ReadNonBlocked(ch)
+	}
+
+	// 检查是否已关闭
+	defer func() {
+		if e := recover(); e != nil {
+			emptyAndClosed = true
+		}
+	}()
+
+	t := time.NewTimer(timeout)
+	defer func() {
+		if !t.Stop() {
+			_, _, _ = ReadNonBlocked(t.C)
+		}
+	}()
+
+	// 读取
+	chanOpened := true
+
+	select {
+	case <-t.C:
+		emptyAndTimeout = true
+	case v, chanOpened = <-ch:
+		// OK
+	}
+
+	if !chanOpened {
+		return v, true, true
+	}
+	return v, emptyAndTimeout, false
 }
--- channel/channel_test.go
+++ channel/channel_test.go
@@ -2,6 +2,7 @@ package channel_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/Andrew-M-C/go.util/channel"
 	"github.com/smartystreets/goconvey/convey"
@@ -11,6 +12,8 @@ var (
 	cv = convey.Convey
 	so = convey.So
 	eq = convey.ShouldEqual
+	lt = convey.ShouldBeLessThan
+	gt = convey.ShouldBeGreaterThan
 
 	isTrue  = convey.ShouldBeTrue
 	isFalse = convey.ShouldBeFalse
@@ -18,9 +21,10 @@ var (
 
 func TestChannel(t *testing.T) {
 	cv("测试 WriteNonBlocked 和 ReadNonBlocked", t, func() { testWriteNonBlockedReadNonBlocked(t) })
+	cv("测试 WriteWithTimeout 和 ReadWithTimeout", t, func() { testWriteWithTimeoutReadWithTimeout(t) })
 }
 
-func testWriteNonBlockedReadNonBlocked(t *testing.T) {
+func testWriteNonBlockedReadNonBlocked(*testing.T) {
 	cv("没有缓冲区的 chan", func() {
 		ch := make(chan struct{})
 
@@ -105,4 +109,77 @@ func testWriteNonBlockedReadNonBlocked(t *testing.T) {
 		so(empty, eq, true)
 		so(closed, eq, true)
 	})
+}
+
+func testWriteWithTimeoutReadWithTimeout(*testing.T) {
+	cv("没有缓冲区的 chan", func() {
+		ch := make(chan int)
+		res := int(0)
+
+		readInMilli(ch, 100, &res)
+		full, closed := channel.WriteWithTimeout(ch, 1, ms(150))
+		so(full, eq, false)
+		so(closed, eq, false)
+		so(res, eq, 1)
+
+		readInMilli(ch, 100, &res)
+		full, closed = channel.WriteWithTimeout(ch, 2, ms(50))
+		so(full, eq, true)
+		so(closed, eq, false)
+
+		// 让上一个 readInMilli 消费完
+		ch <- 3
+		time.Sleep(ms(100))
+		so(res, eq, 3)
+
+		// 测试读超时的情况
+		start := time.Now()
+		writeInMilli(ch, 4, 100)
+		res = <-ch
+		so(time.Since(start), gt, ms(100))
+		so(res, eq, 4)
+
+		start = time.Now()
+		writeInMilli(ch, 5, 50)
+		writeInMilli(ch, 6, 60)
+		writeInMilli(ch, 7, 70)
+		res = <-ch
+		so(res, eq, 5)
+		so(time.Since(start), gt, ms(50))
+		res = <-ch
+		so(res, eq, 6)
+		so(time.Since(start), gt, ms(60))
+		res = <-ch
+		so(res, eq, 7)
+		so(time.Since(start), gt, ms(70))
+
+		// 关闭
+		close(ch)
+		start = time.Now()
+		full, closed = channel.WriteWithTimeout(ch, 2, ms(1000))
+		so(time.Since(start), lt, ms(100))
+		so(full, eq, false)
+		so(closed, eq, true)
+	})
+}
+
+func ms(msec int) time.Duration {
+	return time.Duration(msec) * time.Millisecond
+}
+
+func writeInMilli[T any](ch chan T, v T, msec int) {
+	go func() {
+		time.Sleep(time.Duration(msec) * time.Millisecond)
+		ch <- v
+	}()
+}
+
+func readInMilli[T any](ch chan T, msec int, result ...*T) {
+	go func() {
+		time.Sleep(time.Duration(msec) * time.Millisecond)
+		v := <-ch
+		if len(result) > 0 && result[0] != nil {
+			*result[0] = v
+		}
+	}()
 }
