4d7fe3f266b3ad52607f3ecd900f91eab7892a29
Andrew-M-C <laplacezhang@126.com>
2024-11-09 17:22:35 +0800

    [*] fix: <sync/atomic> Value 的方法接收器应该是一个指针, 修复之

--- sync/atomic/atomic_value.go
+++ sync/atomic/atomic_value.go
@@ -7,22 +7,22 @@ type Value[T any] struct {
 	v atomic.Value
 }
 
-func (v Value[T]) Load() T {
+func (v *Value[T]) Load() T {
 	intf := v.v.Load()
 	res, _ := intf.(T)
 	return res
 }
 
-func (v Value[T]) Store(val T) {
+func (v *Value[T]) Store(val T) {
 	v.v.Store(val)
 }
 
-func (v Value[T]) Swap(new T) (old T) {
+func (v *Value[T]) Swap(new T) (old T) {
 	oldIntf := v.v.Swap(new)
 	old, _ = oldIntf.(T)
 	return old
 }
 
-func (v Value[T]) CompareAndSwap(old, new T) (swapped bool) {
+func (v *Value[T]) CompareAndSwap(old, new T) (swapped bool) {
 	return v.v.CompareAndSwap(old, new)
 }
