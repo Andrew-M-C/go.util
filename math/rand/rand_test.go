eab8e490a7c2e50d7cabfd31c80693dd9c63d08c
Andrew-M-C <laplacezhang@126.com>
2025-10-09 15:18:10 +0800

    [*] test: <math/rand> 逻辑不变. 增加更多单元测试

--- math/rand/rand_test.go
+++ math/rand/rand_test.go
@@ -1,6 +1,8 @@
 package rand_test
 
 import (
+	"fmt"
+	"sync"
 	"testing"
 
 	"github.com/Andrew-M-C/go.util/math/rand"
@@ -21,7 +23,7 @@ func TestRand(t *testing.T) {
 
 func testIndexByRatios(t *testing.T) {
 	cv("基本逻辑", func() {
-		const total = 10000
+		const total = 100000
 		ratios := []int32{11, 22, 33, 44}
 		result := make([]int, len(ratios))
 
@@ -33,14 +35,43 @@ func testIndexByRatios(t *testing.T) {
 
 		t.Logf("result: %v", result)
 
-		so(result[0], between, inRange(1000, 0.15)...)
-		so(result[1], between, inRange(2000, 0.15)...)
-		so(result[2], between, inRange(3000, 0.15)...)
-		so(result[3], between, inRange(4000, 0.15)...)
+		so(result[0], between, inRange(10000, 0.05)...)
+		so(result[1], between, inRange(20000, 0.05)...)
+		so(result[2], between, inRange(30000, 0.05)...)
+		so(result[3], between, inRange(40000, 0.05)...)
+	})
+
+	cv("并发抽奖", func() {
+		const eachTotal = 10000
+		const concurrency = 256
+		ratios := []int32{11, 22, 33, 44}
+		result := make([]int, len(ratios))
+
+		wg := &sync.WaitGroup{}
+		wg.Add(concurrency)
+
+		for c := 0; c < concurrency; c++ {
+			go func() {
+				defer wg.Done()
+				for i := 0; i < eachTotal; i++ {
+					v := rand.IndexByRatios(ratios, func(_ int, v int32) int32 { return v })
+					fmt.Printf("%d", v)
+					result[v]++
+				}
+			}()
+		}
+
+		wg.Wait()
+		t.Logf("result: %v", result)
+
+		so(result[0], between, inRange(1000*concurrency, 0.05)...)
+		so(result[1], between, inRange(2000*concurrency, 0.05)...)
+		so(result[2], between, inRange(3000*concurrency, 0.05)...)
+		so(result[3], between, inRange(4000*concurrency, 0.05)...)
 	})
 
 	cv("部分值为零或者小于零的情况", func() {
-		const total = 10000
+		const total = 100000
 		ratios := []int{-100, 22, 0, 33, 0}
 		result := make([]int, len(ratios))
 
@@ -53,9 +84,9 @@ func testIndexByRatios(t *testing.T) {
 		t.Logf("result: %v", result)
 
 		so(result[0], eq, 0)
-		so(result[1], between, inRange(4000, 0.15)...)
+		so(result[1], between, inRange(40000, 0.05)...)
 		so(result[2], eq, 0)
-		so(result[3], between, inRange(6000, 0.15)...)
+		so(result[3], between, inRange(60000, 0.05)...)
 		so(result[4], eq, 0)
 	})
 }
